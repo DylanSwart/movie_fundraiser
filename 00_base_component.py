5058f593f10fb01f297c7ee0267904bdead2e292
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-01 12:23:37 +1200

    Intro

---
+++ 00_base_component.py
@@ -0,0 +1,12 @@
+# Introduction and instructions
+print("Welcome to the movie fundraiser")
+# Ask if they have used this before
+intro = input("Have you used this program before? ")
+instructions = ("How To Use, \n"
+                "When asked a question please enter what it is asking")
+
+if intro == "Yes":
+    print("Ok welcome back")
+
+elif intro == "No":
+    print(instructions)
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
+name = input("What is your name")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(name)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
