a863da7d1c1b3bfb53dfd965bb01c42e4dce1cb7
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-02 13:46:30 +1200

    Blank function

--- 00_base_component.py
+++ 00_base_component.py
@@ -4,3 +4,20 @@
 print("Welcome to the movie fundraiser \n"
       "Please follow what the program says. \n"
       "there are limit seats of 150 seats available in the theater")
+
+
+# Function goes here
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry this can't be blank")
+
+
+# Main Routine goes here
+name = not_blank("Name: ")
