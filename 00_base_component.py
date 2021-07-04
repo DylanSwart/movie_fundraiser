08758a2628ccecf7b9b089c06b03757d7121a6eb
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-05 09:54:23 +1200

    Ticket info v2

--- 00_base_component.py
+++ 00_base_component.py
@@ -18,3 +18,24 @@ def not_blank(question, error_message):
 name = not_blank("Name: ",
                  "Sorry this can't be blank," 
                  "please enter your name")
+
+# Start Loop
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count <= MAX_TICKETS:
+    print("You have {} seats "
+          "left".format(MAX_TICKETS - count))
+
+    # Get Details
+    name = input("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all the available tickets")
+
+else:
+    print("You have sold {} tickets. \n"
+          "There are still {} tickets left".format(count, MAX_TICKETS - count))
