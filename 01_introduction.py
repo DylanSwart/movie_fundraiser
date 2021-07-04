70488bb2a2fe7562a6d9390ed9a6abb91b9360fc
DylanSwart <86388562+DylanSwart@users.noreply.github.com>
2021-07-05 09:56:15 +1200

    Delete 01_introduction.py

--- 01_introduction.py
+++
@@ -1,5 +0,0 @@
-# Component 1 introduction and instructions
-# introduction to the program
-print("Welcome to the movie fundraiser \n"
-      "Please follow what the program says. \n"
-      "there are limit seats of 150 seats available in the theater")
