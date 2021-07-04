813189eed7dfffd381170d59c00b7c7b13e92f19
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-05 09:28:36 +1200

    Blank function

--- 00_base_component.py
+++ 00_base_component.py
@@ -1,9 +1,4 @@
 # Introduction and instructions
-# Component 1 introduction and instructions
-# introduction to the program
-print("Welcome to the movie fundraiser \n"
-      "Please follow what the program says. \n"
-      "there are limit seats of 150 seats available in the theater")
 
 
 # Function goes here
