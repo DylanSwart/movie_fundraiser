1b3ca078d70a47d06c62469ade057ee7a5158546
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-07 11:47:22 +1200

    Base component

--- 00_base_component.py
+++ 00_base_component.py
@@ -1,41 +1,29 @@
-# Introduction and instructions
+# Import statements
+
+# Functions go here
+
+# Main routine goes here
+
+# Set up dictionaries / lists needed for data
+
+# ask user if they have used this program before
+
+# Loop ticket information
+
+    # Get name can't be blank
+
+    # Get age have to be between 12 and 130
+
+    # calculate ticket price
+
+    # Loop to ask user for snacks
+
+    # Calculate snacks price
+
+    # ask user for payment method and apply surcharge if needed
+
+# Calculate total sales and profit
+
+# Out put data to text file
 
 
-# Not Blank Function goes here
-def not_blank(question, error_message):
-    valid = False
-
-    while not valid:
-        response = input(question)
-
-        if response != "":
-            return response
-        else:
-            print(error_message)
-
-
-# Main Routine goes here
-name = not_blank("Name: ",
-                 "Sorry this can't be blank," 
-                 "please enter your name")
-
-name = ""
-count = 0
-MAX_TICKETS = 5
-
-# Start of Loop
-while name != "xxx" and count <= MAX_TICKETS:
-    print("You have {} seats "
-          "left".format(MAX_TICKETS - count))
-
-    # Get Details
-    name = input("Name: ")
-    count += 1
-    print()
-
-if count == MAX_TICKETS:
-    print("You have sold all the available tickets")
-
-else:
-    print("You have sold {} tickets. \n"
-          "There are still {} tickets left".format(count, MAX_TICKETS - count))
