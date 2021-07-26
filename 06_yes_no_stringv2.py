0c256312351a1b855a1a3bbeff71fc791b37cf45
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-27 09:28:36 +1200

    Snacksv2 bug fix

--- 06_yes_no_stringv2.py
+++ 06_yes_no_stringv2.py
@@ -28,6 +28,6 @@ def string_checker(question, to_check):
 
 for item in range(0, 6):
     want_snacks = string_checker("Do you want "
-                                 "snacks: ", ["Yes", "No"])
+                                 "snacks?: ", ["yes", "no"])
     print("Answer OK, you said: {}".format(want_snacks))
     print()
