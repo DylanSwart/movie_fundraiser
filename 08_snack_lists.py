04b97afb81ab4427b77d098a6e1e8608b3d53c77
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-30 14:04:03 +1200

    snack_list

--- 08_snack_lists.py
+++ 08_snack_lists.py
@@ -21,7 +21,34 @@ snack_menu_dict = {
 
 test_data = [
     [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
+    [[]],
     [[1, 'Pita Chips'], [1, 'Orange Juice'], [2, 'M&Ms']],
     [[1, 'Water']]
 ]
 
+count = 0
+for client_order in test_data:
+
+    # Assume No snacks have been bought
+    for item in snack_lists:
+        item.append(0)
+
+    # Print snack list
+
+    # Get order hard coded for testing
+    snack_order = test_data[count]
+    count += 1
+
+    for item in snack_order:
+        if len(item) > 0:
+            to_find = (item[1])
+            amount = (item[0])
+            add_list = snack_menu_dict[to_find]
+            add_list[-1] = amount
+
+print(snack_lists)
+print("Popcorn: ", snack_lists[0])
+print("M&Ms: ", snack_lists[1])
+print("Pita Chips", snack_lists[2])
+print("Water", snack_lists[3])
+print("Orange Juice", snack_lists[4])
