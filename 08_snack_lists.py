231d324d0d516783c944165d17371b6933cad603
DylanSwart <swart.dylan@awatapu.school.nz>
2021-07-30 13:47:33 +1200

    snack_lists

---
+++ 08_snack_lists.py
@@ -0,0 +1,27 @@
+# Component 7 snack lists
+
+# Initialise snack lists
+names = ['Dylan', 'Bob', 'Jill', 'Bill']
+
+popcorn = []
+mms = []
+pita_chips = []
+water = []
+orange_juice = []
+
+snack_lists = [popcorn, mms, pita_chips, water, orange_juice]
+
+snack_menu_dict = {
+    'Popcorn': popcorn,
+    'Water': water,
+    'M&Ms': mms,
+    'Pita Chips': pita_chips,
+    'Orange Juice': orange_juice
+    }
+
+test_data = [
+    [[1, 'Water'], [1, 'Pita Chips'], [1, 'M&Ms']],
+    [[1, 'Pita Chips'], [1, 'Orange Juice'], [2, 'M&Ms']],
+    [[1, 'Water']]
+]
+
